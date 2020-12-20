36cb296d8371cc4a44909958e3c9358114f68d36
LuisMoncaixa96 <74066200+LuisMoncaixa96@users.noreply.github.com>
2020-12-20 17:39:41 +0000

    Procura artigos

---
+++ Gene ACE2/Procura de artigos ACE2.py
@@ -0,0 +1,24 @@
+from Bio import Entrez
+from Bio import Medline
+Entrez.email = "pg42874@alunos.uminho.pt"
+handle = Entrez.egquery(term = "ACE2")
+record = Entrez.read(handle)
+for row in record["eGQueryResult"]:
+    if row["DbName"]=="pubmed":
+        total = row["Count"]
+
+handle = Entrez.esearch(db = "pubmed", term = "ACE2", retmax=total)
+record = Entrez.read(handle)
+idlist = record["IdList"]
+handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
+records = list(Medline.parse(handle))
+record_results = open("artigos_PubMed.txt", 'w')
+for record in records: 
+    a1 = "Title: " + str(record.get("TI", "?"))
+    a2 = "\nAuthors: " + str(record.get("AU", "?"))
+    a3 = "\nSource: " + str(record.get("SO", "?"))
+    record_results.write(a1)
+    record_results.write(a2)
+    record_results.write(a3)
+    record_results.write("\n\n")
+record_results.close()
